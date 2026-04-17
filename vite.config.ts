ae192c350f60d60e49f51518f7d46f9c7e4f910d
Madhav Singla <madsing8@gmail.com>
2026-04-17 13:56:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+  // http:ver
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
